93b23b9c5789997f7641664c367e1541c8410b0f
Mariella Gottmann <mariellag@mac.boeblingen.de.ibm.com>
2025-06-27 09:49:54 +0200

    Links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <h1>Logikgatter mit Toggles</h1>
 
-<p>And Seite: <a href="/and">svelte.dev/docs/kit</a> </p>
-<p>Or Seite: <a href="/or">svelte.dev/docs/kit</a> </p>
-<p>Invert Seite: <a href="/invert">svelte.dev/docs/kit</a> </p>
-<p>xOR Seite: <a href="/xor">svelte.dev/docs/kit</a> </p>
-<p>nand Seite: <a href="/nand">svelte.dev/docs/kit</a> </p>
-<p>xNOR Seite: <a href="/xnor">svelte.dev/docs/kit</a> </p>
-<p>NOR Seite: <a href="/nor">svelte.dev/docs/kit</a> </p>
+<p>And Seite: <a href="/and">AND</a> </p>
+<p>Or Seite: <a href="/or">OR</a> </p>
+<p>Invert Seite: <a href="/invert">Invert</a> </p>
+<p>xOR Seite: <a href="/xor">xOR</a> </p>
+<p>nand Seite: <a href="/nand">nand</a> </p>
+<p>xNOR Seite: <a href="/xnor">xNOR</a> </p>
+<p>NOR Seite: <a href="/nor">NOR</a> </p>
 <br>
 
 <slot>
